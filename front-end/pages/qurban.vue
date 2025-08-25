16476eca7061400aeb549e6911768ec6892fdefa
syahdan312 <indrasyahdan530@gmail.com>
2025-08-25 08:46:45 +0700

    anjasy

--- front-end/pages/qurban.vue
+++ front-end/pages/qurban.vue
@@ -16,7 +16,7 @@
         </ul>
         <!-- Mobile Menu Button -->
         <button class="md:hidden p-2" @click="toggleMobileMenu">
-          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"> 
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
