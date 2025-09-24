0e45068b5236ab3a1ca1e4280f07d503ab7886a6
artha <arthaparulian31@gmail.com>
2025-09-24 09:11:06 +0700

    asdawdas

--- back-end-api/src/donation/donation.module.ts
+++ back-end-api/src/donation/donation.module.ts
@@ -2,11 +2,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Donation } from './entities/donation.entity';
+import { DonationService } from './donation.service';
+import { DonationController } from './donation.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Donation])],
-  controllers: [],
-  providers: [],
+  controllers: [DonationController],
+  providers: [DonationService],
   exports: [TypeOrmModule], // supaya bisa dipakai di ProgramModule
 })
 export class DonationModule {}
