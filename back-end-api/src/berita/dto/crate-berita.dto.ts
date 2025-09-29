635480e0c943765bad3342109b3824c7a74fa740
Mayiyooo <muhamadmario619@gmail.com>
2025-09-29 14:24:01 +0700

    yayaya

--- back-end-api/src/berita/dto/crate-berita.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateBeritaDto {
-  title: string;       // Judul berita
-  content: string;     // Isi berita
-  category: string;    // Kategori berita (Politik, Ekonomi, dll)
-  image?: string;      // URL gambar berita (opsional)
-  created_at?: Date;   // Tanggal berita dibuat
-}
